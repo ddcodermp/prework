b50ec4bd294912c05914a90d4fe08adbce847e19
Milton Pettus <mpettus@cnm.edu>
2020-03-20 14:48:30 -0600

    Added Starter index page

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+	  <meta charset="UTF-8"/>
+	  <title>I Completed the Prework</title>
+ </head>
+ <body>
+   <p>I Completed the prework!</p>
+ <body>
+ </html>
+
